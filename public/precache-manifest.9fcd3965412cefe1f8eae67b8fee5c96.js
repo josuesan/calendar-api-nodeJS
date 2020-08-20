1fdc0ebd8ec7bdc178c6c09b21033f7db1bfdec2
josuesan <josueguitar2@gmail.com>
2020-08-20 16:36:51 -0400

    app included in public

---
+++ public/precache-manifest.9fcd3965412cefe1f8eae67b8fee5c96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30798081f8c5d798ffe878ea03126e7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbc262346134c97a32e1",
+    "url": "/static/css/2.cd9b3f80.chunk.css"
+  },
+  {
+    "revision": "2fa17c704bbfa8ada664",
+    "url": "/static/css/main.2e8b9bb6.chunk.css"
+  },
+  {
+    "revision": "dbc262346134c97a32e1",
+    "url": "/static/js/2.8ddb21ce.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.8ddb21ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa17c704bbfa8ada664",
+    "url": "/static/js/main.243b01a0.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
